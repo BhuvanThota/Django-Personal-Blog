3ce5303f201ad26c4a2a02cade70440fc6ca3c4b
BhuvanThota <teacherb3337@gmail.com>
2024-10-04 06:35:56 +0530

    Update settings.py
    debug  to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1rs1j=ih(lo)f3sp^xys-r%ol!+9+4oc(t!a2m3(v3megc)ik&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = []
 
